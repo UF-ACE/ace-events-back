b7a2886316a7d61993fc915b917866720fa98c6a
Christopher Miller <admin@mctherealm.net>
2020-01-14 22:45:21 -0500

    Removed back developer callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  post '/auth/developer/callback', to: 'sessions#create'
   post '/auth/ace_cloud/callback', to: 'sessions#create'
   get '/auth/logout', to: 'sessions#destroy'
 
